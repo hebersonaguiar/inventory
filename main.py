062af55562f0a120c3552e65f642999d1292ea09
hebersonaguiar <hebersonaguiar@gmail.com>
2023-10-24 19:38:53 -0300

    First test

---
+++ main.py
@@ -0,0 +1,32 @@
+import json, re, csv
+from flask import Flask, render_template, request, redirect, url_for, flash, stream_with_context, g, session
+from flask_restful import Resource, Api
+from flask_jsonpify import jsonify
+from json import dumps
+from datetime import datetime
+from io import StringIO
+from flask_cors import CORS
+from repositories import connection
+
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.secret_key = "flash message"
+
+@app.route('/test', methods=['GET'])
+def test():
+    try:
+        cur = connection.get_connection()
+        cur.execute("select * from test")
+        data = cur.fetchall()
+
+        return jsonify({'test': 'true'}), 200
+    except Exception as error:
+        return jsonify(error), 400
+    finally:
+        cur.close
+        
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port='5000')
---
+++ repositories/connection.py
@@ -0,0 +1,16 @@
+from flask_mysqldb import MySQL
+import app
+
+def get_connection():
+    app.config['MYSQL_HOST'] = 'mysql'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'my-secret-pw'
+    app.config['MYSQL_DB'] = 'inventory'
+
+    mysql = MySQL(app)
+
+    cur = mysql.connection.cursor()
+    
+    return cur
+
+
