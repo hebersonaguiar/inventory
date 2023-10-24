9e4f5d1ba90a677c865cba6053268c684b8be7cb
hebersonaguiar <hebersonaguiar@gmail.com>
2023-10-24 20:56:00 -0300

    Change files

--- main.py
+++ main.py
@@ -4,27 +4,36 @@ from flask import Flask, render_template, request, redirect, url_for, flash, str
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from flask_jsonpify import jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = "flash message"
 
+app.config['MYSQL_HOST'] = "mysql"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "my-secret-pw"
+app.config['MYSQL_DB'] = "inventory"
+
+mysql = MySQL(app)
+
 @app.route('/test', methods=['GET'])
 def test():
     try:
         print("Passei pelo try")
-        cur = connection.get_connection()
-        cursor = cur.connection.cursor()
-        cursor.execute("select * from test")
-        data = cursor.fetchall()
+        # cur = connection.get_connection()
+        cur = mysql.connection.cursor()
+        # cursor = cur.connection.cursor()
+        cur.execute("select * from test")
+        data = cur.fetchall()
 
         return jsonify({'test': 'true'}), 200
     except Exception as error:
         print("Passei pela ececao do try")
         return jsonify(error), 400
     finally:
-        cursor.close
+        cur.close
         
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port='5000')
