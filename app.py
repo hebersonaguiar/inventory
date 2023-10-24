52eb68466727f45cec9ab1004731fc30995702c3
hebersonaguiar <hebersonaguiar@gmail.com>
2023-10-24 20:22:37 -0300

    Change files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, stream_with_context, g, session
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
