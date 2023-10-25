7f2e9d5ceee547ccc1cdfbaa3058acbfff3ed69b
hebersonaguiar <hebersonaguiar@gmail.com>
2023-10-24 21:35:18 -0300

    Change files

--- main.py
+++ main.py
@@ -10,25 +10,22 @@ api = Api(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = "flash message"
 
-pdb.init_app(app)
-
-mysql = connection.get_connection()
+mysql = connection.get_connection(app)
 
 @app.route('/test', methods=['GET'])
 def test():
     try:
         print("Passei pelo try")
-        cur = pdb.connect(app)
-        cursor = cur.connection.cursor()
-        cursor.execute("select * from test")
-        data = cursor.fetchall()
+        cur = mysql.connection.cursor()
+        cur.execute("select * from test")
+        data = cur.fetchall()
 
         return jsonify({'test': 'true'}), 200
     except Exception as error:
         print("Passei pela ececao do try")
         return jsonify(error), 400
     finally:
-        cursor.close
+        cur.close
         
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port='5000')
--- repositories/connection.py
+++ repositories/connection.py
@@ -1,35 +1,14 @@
 from flask_mysqldb import MySQL
-# import app
 
+def get_connection(app):
+    print("Connections")
+    app.config['MYSQL_HOST'] = "mysql"
+    app.config['MYSQL_USER'] = "root"
+    app.config['MYSQL_PASSWORD'] = "my-secret-pw"
+    app.config['MYSQL_DB'] = "inventory"
 
-class MysqlDB:
-    def __init__(self):
-        self.app = None
+    mysql = MySQL(app)
+
+    # cur = mysql.connection.cursor()
     
-    def ini_app(self,app):
-        self.app = app
-        self.connect()
-
-    def connect(self):
-        self.app.config['MYSQL_HOST'] = "mysql"
-        self.app.config['MYSQL_USER'] = "root"
-        self.app.config['MYSQL_PASSWORD'] = "my-secret-pw"
-        self.app.config['MYSQL_DB'] = "inventory"
-
-        return self.app
-
-
-# def get_connection():
-#     print("Connections")
-#     app.app.config['MYSQL_HOST'] = "mysql"
-#     app.app.config['MYSQL_USER'] = "root"
-#     app.app.config['MYSQL_PASSWORD'] = "my-secret-pw"
-#     app.app.config['MYSQL_DB'] = "inventory"
-
-#     mysql = MySQL(app.app)
-
-#     # cur = mysql.connection.cursor()
-    
-#     return mysql
-
-
+    return mysql
