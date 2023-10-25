42f4978508ba1f714f794aefcdc5a423b6e0932d
hebersonaguiar <hebersonaguiar@gmail.com>
2023-10-24 21:09:37 -0300

    Change files

--- repositories/connection.py
+++ repositories/connection.py
@@ -3,10 +3,10 @@ import app
 
 def get_connection():
     print("Connections")
-    app.config['MYSQL_HOST'] = "mysql"
-    app.config['MYSQL_USER'] = "root"
-    app.config['MYSQL_PASSWORD'] = "my-secret-pw"
-    app.config['MYSQL_DB'] = "inventory"
+    app.app.config['MYSQL_HOST'] = "mysql"
+    app.app.config['MYSQL_USER'] = "root"
+    app.app.config['MYSQL_PASSWORD'] = "my-secret-pw"
+    app.app.config['MYSQL_DB'] = "inventory"
 
     mysql = MySQL(app)
 
