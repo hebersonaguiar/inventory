153eadd6356915fda51a3e3ae8c953789dba5ab2
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-05-18 17:58:17 -0300

    Add consumer process

--- consumer.py
+++ consumer.py
@@ -31,5 +31,15 @@ channel.queue_declare(queue='infrasa_inventory_queue', durable=True)
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='infrasa_inventory_queue', on_message_callback=process_message)
 
-print(' [*] Aguardando mensagens. Para sair pressione CTRL+C')
-channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        print('[*] Aguardando mensagens. Pressione CTRL+C para sair.')
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print('[!] Encerrando consumidor.')
+        channel.stop_consuming()
+        connection.close()
+
+# print(' [*] Aguardando mensagens. Para sair pressione CTRL+C')
+# channel.start_consuming()
