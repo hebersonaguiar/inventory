c4e806ad1ac973cabcdd8ac9a1ba1b5f68291eff
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-05-18 18:04:24 -0300

    Add consumer process

--- consumer.py
+++ consumer.py
@@ -25,21 +25,21 @@ def process_message(ch, method, properties, body):
     except Exception as e:
         print(f"[✗] Erro ao processar mensagem: {e}")
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-channel = connection.channel()
-channel.queue_declare(queue='infrasa_inventory_queue', durable=True)
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='infrasa_inventory_queue', on_message_callback=process_message)
+# connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+# channel = connection.channel()
+# channel.queue_declare(queue='infrasa_inventory_queue', durable=True)
+# channel.basic_qos(prefetch_count=1)
+# channel.basic_consume(queue='infrasa_inventory_queue', on_message_callback=process_message)
 
 
-if __name__ == '__main__':
-    try:
-        print('[*] Aguardando mensagens. Pressione CTRL+C para sair.')
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        print('[!] Encerrando consumidor.')
-        channel.stop_consuming()
-        connection.close()
+# if __name__ == '__main__':
+#     try:
+#         print('[*] Aguardando mensagens. Pressione CTRL+C para sair.')
+#         channel.start_consuming()
+#     except KeyboardInterrupt:
+#         print('[!] Encerrando consumidor.')
+#         channel.stop_consuming()
+#         connection.close()
 
 # print(' [*] Aguardando mensagens. Para sair pressione CTRL+C')
 # channel.start_consuming()
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import app
+import pika
+from consumer import process_message
 
 app = app.application
 
-if __name__ == '__main__':
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
+channel.queue_declare(queue='infrasa_inventory_queue', durable=True)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='infrasa_inventory_queue', on_message_callback=process_message)
+
+def run_app():
 	app.run(debug=True, host='0.0.0.0', port='5000')
+
+if __name__ == '__main__':
+	try:
+		print('[*] Aguardando mensagens. Pressione CTRL+C para sair.')
+		channel.start_consuming()
+		run_app()
+	except KeyboardInterrupt:
+		print('[!] Encerrando consumidor.')
+		channel.stop_consuming()
+		connection.close()
