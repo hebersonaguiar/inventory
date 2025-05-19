57d5de5ea86875773f1205bb76587a4562d61496
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-05-18 23:05:21 -0300

    Add consumer process

--- repositories/connection.py
+++ repositories/connection.py
@@ -30,7 +30,7 @@ def get_rabbitmq_connection():
 
     parameters = pika.ConnectionParameters(
         host=os.getenv("RABBITMQ_HOST"),
-        port=int(os.getenv("RABBITMQ_PORT")),
+        port=os.getenv("RABBITMQ_PORT"),
         credentials=credentials
     )
 
