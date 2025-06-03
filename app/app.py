bccf903aaf95021af7aa45f331241797c11ab11f
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-06-02 23:29:10 -0300

    New files and configs refactoring

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ from app.interface.api import register_blueprints
 from app.config.settings import Settings
 
 application = Flask(__name__)
-application.settings.from_mapping(Settings())
+application.config.from_mapping(Settings())
 
 register_blueprints(application)
