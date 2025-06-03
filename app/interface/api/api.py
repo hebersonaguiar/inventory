497ed207c12c29c116c51224100aae0a7f6be6d7
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-06-02 23:41:27 -0300

    New files and configs refactoring

--- app/interface/api/api.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from app.interface.routes.routes_inventory import inventory_bp
-# from .auth_routes import auth_bp  # future
-
-def register_blueprints(app: Flask):
-    app.register_blueprint(inventory_bp)
