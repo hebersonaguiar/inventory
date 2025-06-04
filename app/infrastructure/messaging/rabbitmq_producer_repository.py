350b37a1ce21e3813d98126dd5fb70eafa782c20
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-06-03 23:15:35 -0300

    Add new route to queue

---
+++ app/infrastructure/messaging/rabbitmq_producer_repository.py
@@ -0,0 +1,27 @@
+from app.infrastructure.messaging.rabbitmq_connection import get_rabbitmq_connection
+import json
+import os
+import pika
+
+class RabbitMQInventoryRepository:
+
+    def __init__(self):
+        self.queue_name = os.getenv("RABBITMQ_QUEUE", "infrasa_inventory_queue")
+
+    def send_to_queue(self, data: dict) -> str:
+        connection = get_rabbitmq_connection()
+        channel = connection.channel()
+
+        channel.queue_declare(queue=self.queue_name, durable=True)
+        message = json.dumps(data)
+
+        channel.basic_publish(
+            exchange='',
+            routing_key=self.queue_name,
+            body=message,
+            properties=pika.BasicProperties(
+                delivery_mode=2, # Torna a mensagem persistente
+            )
+        )
+
+        connection.close()
